eccad873d0cd52e97abd435f6519686d9276e6ef
LingGrani <luthfiangger2510@gmail.com>
2023-05-18 20:22:04 +0700

    jika data tidak kosong, masukan nim untuk mencari data

--- 137_singlelinkedlist/137_singlelinkedlist.cpp
+++ 137_singlelinkedlist/137_singlelinkedlist.cpp
@@ -109,6 +109,23 @@ void searchData() {
         system("cls");
         return;
     }
+    else
+    {
+        int nim;
+        cout << "Masukan NIM: ";
+        cin >> nim;
+        node* currentNode = START;
+        while (currentNode != NULL)
+        {
+            if (currentNode->noMhs == nim)
+            {
+                cout << "NIM" << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+                return;
+            }
+            currentNode = currentNode->next;
+        }
+        cout << "Data tidak ditemukan" << endl;
+    }
 }
 
 int main()
